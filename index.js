2546a5093c9bb51a78f20133dec2f4bed55c02c6
kdawg <mcleod.karla@gmail.com>
2020-05-24 11:00:09 -0400

    initial commit with files from develop folder, as well as psudocode from covering homework startup

---
+++ index.js
@@ -0,0 +1,36 @@
+// dont forget: npm init
+
+// The user will be prompted for their GitHub username and other information pertaining to the project the README is for
+
+
+const questions = [
+
+    /* This is going to an array of questions:
+ 
+     what is your Project title? [string]
+     What is your description? [string]
+     What is your table of contents? [array:string] or an object
+     How to install?  [string]
+     What is your usgae?  [string]
+     What is your License? "MIT"  [string]
+     Who are the contributors? [array:string] ["karla", "dave", " dan"]
+     What is your Tests? [string] markdown formatted?
+     What is your Questions? see below:
+         What is your User Github profil picture?
+         What is your use github email? */
+
+
+];
+
+function writeToFile(fileName, data) {
+    // where do I want the file to be placed? desktop? local directory? do I need to check?
+    // create a file with name fileName
+    // write to file fileName the data 
+}
+
+function init() {
+    // initialize stuff that I need here
+
+}
+
+init();
