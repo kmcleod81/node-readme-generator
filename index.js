c811b56d1f431431919ce47439da6606785bc948
kdawg <mcleod.karla@gmail.com>
2020-05-30 07:12:39 -0400

    added questions using inquirer, and added require to writeFile

--- index.js
+++ index.js
@@ -1,32 +1,99 @@
 // dont forget: npm init
 
-// The user will be prompted for their GitHub username and other information pertaining to the project the README is for
+// The user will be prompted for their GitHub username and other information pertaining to the project the README is for:
+
+const inquirer = require('inquirer');
+//This is going to an array of questions:
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'desc',
+        message: 'What is your description?',
+    },
+    {
+        type: 'input',
+        name: 'toc',
+        message: 'What is your table of contents?',
+    },
+    {
+        type: 'input',
+        name: 'howToInstall',
+        message: 'How to install the application?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your usage?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What is your license?',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who are the contributors?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are your tests?',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?',
+    },
+    {
+        type: 'input',
+        name: 'pic',
+        message: 'What is your Github profile picture?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your Github email?',
+    },
+    {
+        type: 'list',
+        name: 'badges',
+        message: 'Choose a badge:',
+        choices: [
+            'Awesome Badges',
+            //[![Awesome Badges](https://img.shields.io/badge/badges-awesome-green.svg)](https://github.com/Naereen/badges)
+
+            'Custom Badge',
+            //[![Awesome Badges](https://img.shields.io/badge/badges-awesome-green.svg)](https://github.com/Naereen/badges)
+
+        ]
+    },
+])
+    .then((answers) => {
+        console.log(answers);
+    });
 
 
-const questions = [
 
-    /* This is going to an array of questions:
- 
-     what is your Project title? [string]
-     What is your description? [string]
-     What is your table of contents? [array:string] or an object
-     How to install?  [string]
-     What is your usgae?  [string]
-     What is your License? "MIT"  [string]
-     Who are the contributors? [array:string] ["karla", "dave", " dan"]
-     What is your Tests? [string] markdown formatted?
-     What is your Questions? see below:
-         What is your User Github profil picture?
-         What is your use github email? */
+// function writeToFile(fileName, data) {
+// where do I want the file to be placed? desktop? local directory? do I need to check?
+// create a file with name fileName
+// write to file fileName the data 
+// }
 
+var fs = require('fs');
 
-];
-
-function writeToFile(fileName, data) {
-    // where do I want the file to be placed? desktop? local directory? do I need to check?
-    // create a file with name fileName
-    // write to file fileName the data 
-}
+fs.writeFile('README.md', process.argv[2], function (err) {
+    if (err) {
+        throw err;
+    }
+});
 
 function init() {
     // initialize stuff that I need here
@@ -34,3 +101,6 @@ function init() {
 }
 
 init();
+
+
+
